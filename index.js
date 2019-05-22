fc3a3c8686acacff0d8d97ba522fe2495bf1234f
Andrew Ruether <aruethe2@swarthmore.edu>
2019-05-22 10:01:12 -0400

    Enable port specification

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var http = require('http');
 var swaggerTools = require('swagger-tools');
 var jsyaml = require('js-yaml');
 var fs = require('fs');
-var serverPort = 8080;
+var serverPort = process.env.PORT || 8080;
 
 // swaggerRouter configuration
 var options = {
